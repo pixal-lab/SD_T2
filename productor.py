ef6b504b2f2e50b328e4da91397fd9fe38441d04
imanjarufe <104101085+imanjarufe@users.noreply.github.com>
2023-10-30 21:23:52 -0300

    Add files via upload

--- productor.py
+++ productor.py
@@ -1,21 +1,54 @@
+from kafka import KafkaProducer
+from json import dumps
+import threading
+import argparse
 import time
 import random
+import string
+
+servidores_bootstrap = 'kafka:9092'
+topic_inscripcion = 'inscripcion'
+topic_ventas = 'ventas'
+topic_reposicion = 'reposicion'
+
+
+productor = KafkaProducer(bootstrap_servers=[servidores_bootstrap])
+
+def generar_id():
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=random.randint(1, 20)))
 
 # Definición de la clase Productor
 class Productor:
-    def __init__(self, frecuencia_ventas, max_stock):
+    def __init__(self, frecuencia_ventas, max_stock, nombre, correo, premium):
         # Constructor de la clase Productor
         self.frecuencia_ventas = int  # Frecuencia de ventas
         self.stock = 10  # Stock inicial
         self.max_stock = max_stock  # Stock máximo
-        self.gratis_pagado = False  # Bandera para gratis o pagado (no se utiliza en este ejemplo)
+        self.premium = False  # Bandera para gratis o pagado (no se utiliza en este ejemplo)
+        self.nombre = str
+        self.correo = str
 
-    def solicitar_inscripcion(self):
-        # Método que simula el proceso de solicitud de inscripción
-        print("Solicitando inscripción...")
-        # Supongamos que aquí se enviaría la solicitud y se esperaría la respuesta.
+    def solicitar_inscripcion(self, nombre, correo, premium):
+        topic = topic_inscripcion
+        mensaje = {
+            "nombre": nombre,
+            "correo": correo,
+            "premium": True
+        }
+        json_mensaje = dumps(mensaje).encode('utf-8')
+        productor.send(topic, json_mensaje)
+        time.sleep(3)
 
-    def vender(self):
+    def vender(self, correo, valor, stock):
+        topic = topic_ventas
+        mensaje = {
+            "correo": correo,
+            "valor": 30000
+        }
+        json_mensaje = dumps(mensaje).encode('utf-8')
+        productor.send(topic, json_mensaje)
+        time.sleep(3)
+
         # Método para simular una venta
         if self.stock > 0:
             self.stock -= 1
@@ -23,14 +56,24 @@ class Productor:
         else:
             print("No hay suficiente stock para vender.")
 
-    def solicitar_reposicion(self):
+
+    def solicitar_reposicion(self, correo):
+        topic = topic_reposicion 
+        mensaje = {
+            "correo": correo,
+        }
+        json_mensaje = dumps(mensaje).encode('utf-8')
+        productor.send(topic, json_mensaje)
+        time.sleep(3)
         # Método que simula el proceso de solicitud de reposición de stock
         print("Solicitando reposición...")
         self.stock = self.max_stock  # Reponer el stock al máximo
 
+
     def leer_correo(self):
         # Método que simula la lectura de correos y revisa estadísticas
         print("Leyendo correo y revisando estadísticas...")
+        #recibe el nombre del correo y algun metodo para identificarlo.
 
     def run(self):
         # Método principal que ejecuta las acciones del Productor
@@ -45,11 +88,7 @@ class Productor:
             contador += 1
             time.sleep(1)  # Esperar 1 segundo entre iteraciones
 
-# Uso del Productor
-frecuencia_ventas = 5
-max_stock = 10
-productor = Productor(frecuencia_ventas, max_stock)
-productor.run()
+
 
 
 
